1ed1b6ab2be397ee0fcb185b0d9fbea1d2a05d8e
Sweta Jain <sweta.jain@neosoftmail.com>
2022-07-11 00:46:14 +0530

    added favorite buttons

--- lib/main.dart
+++ lib/main.dart
@@ -34,10 +34,13 @@ class _RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<_RandomWords> {
-  final _suggestions = <WordPair>[]; // NEW
+  final _suggestions = <WordPair>[];
+  final _saved = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18);
+
   // Used to generate random integers
   final _random = Random();
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -49,16 +52,31 @@ class _RandomWordsState extends State<_RandomWords> {
         if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
+        final alreadySaved = _saved.contains(_suggestions[index]);
+
         return ListTile(
-          title: Text(
-              _suggestions[index].asPascalCase,
-              style: TextStyle(color:  Colors.primaries[_random.nextInt(Colors.primaries.length)]
-              [_random.nextInt(9) * 100],
-                  fontSize: 24)
+          title: Text(_suggestions[index].asPascalCase,
+              style: TextStyle(
+                  color:
+                      Colors.primaries[_random.nextInt(Colors.primaries.length)]
+                          [_random.nextInt(9) * 100],
+                  fontSize: 24)),
+          trailing: Icon(
+            alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null,
+            semanticLabel: alreadySaved ? 'Remove from Saved' : 'Save',
           ),
+          onTap: () {
+            setState(() {
+              if (alreadySaved) {
+                _saved.remove(_suggestions[index]);
+              } else {
+                _saved.add(_suggestions[index]);
+              }
+            });
+          },
         );
       },
     );
   }
 }
-
